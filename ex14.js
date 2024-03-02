32b47881d437754bdad312adeb50228f9c936888
Asma Shahid <asmashahid795@gmail.com>
2024-03-03 01:55:47 +0500

    Add files via upload

---
+++ ex14.js
@@ -0,0 +1,4 @@
+var guestLeast = ["Ali", "Dua", "Saba"];
+for (var i = 0; i < guestLeast.length; i++) {
+    console.log("Dear friend, ' + guestLeast[i] + ',\n\n it is my pleasure to invite  you for dinner.\n\nThank You");
+}
---
+++ ex14.ts
@@ -0,0 +1,6 @@
+let guestLeast: string[] = ["Ali", "Dua", "Saba"];
+for (let i = 0; i < guestLeast.length; i++) {
+  console.log(
+    "Dear friend, ' ${guestLeast[i]} ,\n\n it is my pleasure to invite  you for dinner.\n\nThank You"
+  );
+}
